841ea078ee6f1bf853e4679373c6afeb10e9ef3c
23wh1a0529 <23wh1a0529@bvrithyderabad.edu.in>
2026-04-10 18:44:59 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import TaskList from('/tasklist')
 
 function App() {
   return (
     <div>
       <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<TaskList />} />
+        </Routes>
+      </Router>
 
       {/* TODO: Replace this placeholder with Routes */}
     </div>
